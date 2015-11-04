4f1e273b4d38c5ffc421bc99a758d6c5f99e1268
austek-mdsol <austek@mdsol.com>
2015-11-04 14:54:41 +0000

    Initial commit

---
+++ src/main/java/com/facebook/swift/generator/swift2thrift/PublicSwift2ThriftGenerator.java
@@ -0,0 +1,9 @@
+package com.facebook.swift.generator.swift2thrift;
+
+import java.io.FileNotFoundException;
+
+public class PublicSwift2ThriftGenerator extends Swift2ThriftGenerator {
+    public PublicSwift2ThriftGenerator(Swift2ThriftGeneratorConfig config) throws FileNotFoundException {
+        super(config);
+    }
+}
